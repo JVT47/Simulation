8699f54452a54b8274dd2d5a275c79c8bddb4c6e
Joona <joona.tammela@gmail.com>
2023-01-09 16:15:58 +0200

    MonteCarlo now simulates return distributions for stocks

--- monteCarlo.py
+++ monteCarlo.py
@@ -2,7 +2,7 @@ import pickle
 from returns import Stock
 import numpy as np
 import pandas as pd
-from matplotlib import pyplot
+from matplotlib import pyplot as plt
 
 def inverse_cdf(y, sorted_array):
     probability_step = 1/(sorted_array.size-1)
@@ -20,13 +20,23 @@ with open(save_location, 'rb') as f:
     stock_list = pickle.load(f)
 
 
-omxhpi.log_returns = omxhpi.log_returns.sort_values(by='Closing price', ignore_index = True)
-e = inverse_cdf(np.linspace(0,1,1000, endpoint=False), np.array(omxhpi.log_returns['Closing price']))
+omxhpi.log_returns = omxhpi.log_returns.sort_values(by='Closing price', ignore_index=True)
 
-fig, ax = pyplot.subplots(2)
-ax[0].hist(omxhpi.log_returns['Closing price'])
-ax[1].hist(e)
+index_simulation = np.random.rand(1000,60)
+index_simulation = inverse_cdf(index_simulation, np.array(omxhpi.log_returns['Closing price']))
+index_simulation = np.sum(index_simulation, axis=1)
 
-pyplot.show()
+for n, stock in enumerate(stock_list):
+    stock.log_returns = stock.log_returns.sort_values(by='Closing price', ignore_index=True)
 
+    stock_simulation = np.random.rand(1000, 60)
+    stock_simulation = inverse_cdf(stock_simulation, np.array(stock.log_returns['Closing price']))
+    stock_simulation = np.sum(stock_simulation, axis=1)
 
+    final_distribution = index_simulation + stock_simulation
+
+    ax = plt.subplot(2,2, n+1)
+    ax.hist(final_distribution)
+    ax.set_title(stock.name)
+
+plt.show()
